b9e0e537e8704400ced1f664c2aa84152d4ed874
Chris Bryant <chris_bryant@mac.com>
2020-07-30 16:25:01 -0700

    Added script so that classes change based on current hour

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,12 +1,27 @@
-// Adds current date and time to header
+// Variables
 
+// Checks current time
+var currentHour = moment().hours();
+
+
+// Adds current date and time to header
 $("#currentDay").text(moment().format("dddd MMMM Do, YYYY"));
 $("#currentTime").text(moment().format("LTS"));
 
 
 
-// Checks current time
+// Changes classes of hours
+$(".time-block").each(function () {
+var hour = $(this).attr("id");
+var checkHourID = parseInt(hour);
 
-var currentHour = moment().hours();
-
-console.log(currentHour)
+if (checkHourID < currentHour) {
+    $(this).addClass("past");
+}
+if (checkHourID > currentHour) {
+    $(this).addClass("future");
+}
+if (checkHourID === currentHour) {
+    $(this).addClass("present");
+}
+})
