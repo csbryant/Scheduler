e49e6602f479f18c609daf0874b6088556f3ddc9
Chris Bryant <chris_bryant@mac.com>
2020-07-31 14:59:47 -0700

    Updated script to pull local storage

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,27 +1,37 @@
-// Variables
+// Pulls local storage
+for (let index = 9; index <= 17; index++) {
+  var textArea = $("#" + index).find("textarea");
+  var storage = localStorage.getItem(index);
+  textArea.val(storage);
+}
 
 // Checks current time
 var currentHour = moment().hours();
-
 
 // Adds current date and time to header
 $("#currentDay").text(moment().format("dddd MMMM Do, YYYY"));
 $("#currentTime").text(moment().format("LTS"));
 
+// Saves hour and description in local storage
+$(".saveBtn").on("click", function (event) {
+  var hour = $(this).parent().attr("id");
+  var description = $(this).siblings(".description").val();
 
+  localStorage.setItem(hour, description);
+});
 
-// Changes classes of hours
+// Changes classes of time blocks
 $(".time-block").each(function () {
-var hour = $(this).attr("id");
-var checkHourID = parseInt(hour);
+  var hour = $(this).attr("id");
+  var checkHourID = parseInt(hour);
 
-if (checkHourID < currentHour) {
+  if (checkHourID < currentHour) {
     $(this).addClass("past");
-}
-if (checkHourID > currentHour) {
+  }
+  if (checkHourID > currentHour) {
     $(this).addClass("future");
-}
-if (checkHourID === currentHour) {
+  }
+  if (checkHourID === currentHour) {
     $(this).addClass("present");
-}
-})
+  }
+});
--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 # Credits
 Grabbed time using Moment.JS
 https://momentjs.com/docs/
+Used Moment.JS Tutorial by Dcode
+https://www.youtube.com/watch?v=n80RRNS1k64
